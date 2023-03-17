bb6b91b0540938c183e2298c98c8eeaac564a069
kianyang_lee <ken.lee.kianyang@gmail.com>
2023-03-17 09:48:33 +0800

    ch4 ex2 and ex3 solution

--- ch4/ex2/main.go
+++ ch4/ex2/main.go
@@ -1,8 +1,35 @@
 package main
 
+import (
+	"bufio"
+	"crypto/sha256"
+	"crypto/sha512"
+	"flag"
+	"fmt"
+	"os"
+)
+
 /*
-TODO:
-Exercis e 4.2: Write a program that prints the SHA256 hash of its standard input by defau lt but
+Exercis e 4.2: Write a program that prints the SHA256 hash of its standard input by default but
 supports a command-line flag to print the SHA384 or SHA512 hash instead.
 */
-func main() {}
+
+var shaType = flag.String("sh", "256", "type of sha (256, 384, or 512) for encryption")
+
+func main() {
+	flag.Parse()
+	reader := bufio.NewScanner(os.Stdin)
+
+	reader.Scan()
+	input := reader.Bytes()
+	if *shaType == "256" {
+		checksum := sha256.Sum256(input)
+		fmt.Println(checksum)
+	} else if *shaType == "384" {
+		checksum := sha512.Sum512_256(input)
+		fmt.Println(checksum)
+	} else if *shaType == "512" {
+		checksum := sha512.Sum512(input)
+		fmt.Println(checksum)
+	}
+}
--- ch4/ex3/main.go
+++ ch4/ex3/main.go
@@ -1,3 +1,26 @@
 package main
 
-// Exercis e 4.3: Re write reverse to use an array pointer instead of a slice.
+import "fmt"
+
+// Exercise 4.3: Rewrite reverse to use an array pointer instead of a slice.
+func reverse(slice []int) {
+	for i, j := 0, len(slice)-1; i < j; i, j = i+1, j-1 {
+		slice[i], slice[j] = slice[j], slice[i]
+	}
+}
+
+func reverseArr(arr *[5]int) {
+	for i, j := 0, len(arr)-1; i < j; i, j = i+1, j-1 {
+		arr[i], arr[j] = arr[j], arr[i]
+	}
+}
+
+func main() {
+	a := []int{1, 2, 3, 4, 5}
+	reverse(a)
+	fmt.Println(a) // 5,4,3,2,1
+
+	b := [...]int{1, 2, 3, 4, 5}
+	reverseArr(&b)
+	fmt.Println(b)
+}
